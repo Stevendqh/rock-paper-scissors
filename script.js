9e6777c0c2fe8ece97811b23469fb07239a3f28b
Steven Hoang <Stevendqhoang@Gmail.com>
2023-09-05 20:18:51 -0700

    create function playRound
    Play one round of Rock Paper Scissors with the computer

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-function getComputerChoice(){
+function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3);
 
     switch (randomNum) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors"
+            return "scissors"
         }
     }
+function playRound(playerSelection,computerSelection) {
+    //let playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors");
+    
+    playerSelection = playerSelection.toLowerCase();
 
-console.log("The computer chose: " + getComputerChoice());
-console.log("Hello World");
+    const outcomes = {
+        rock: { beats: "scissors", losesTo: "paper" },
+        paper: { beats: "rock", losesTo: "scissors" },
+        scissors: { beats: "paper", losesTo: "rock" },
+    };
+
+    console.log("You chose: " + playerSelection);
+    console.log("Computer chose: " + computerSelection);
+
+    if (!outcomes[playerSelection] || !outcomes[computerSelection]) {
+        return "Invalid selections. Please choose Rock, Paper, Or Scissors."
+    }
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie! You both chose " + playerSelection + ".";
+    } else if (outcomes[playerSelection].beats === computerSelection) {
+        return "You Win! " + playerSelection + " beats " + computerSelection + ".";
+    } else {
+        return "You lose! " + computerSelection + " beats " + playerSelection + ".";
+    }
+
+}
+
+console.log(playRound("Scissors",getComputerChoice()));
