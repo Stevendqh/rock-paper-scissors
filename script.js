de7fb5a2bdcb442a56efdab0c3f511ed28232e9b
Steven Hoang <Stevendqhoang@Gmail.com>
2023-09-05 19:29:05 -0700

    create function getComputerChoice

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    const randomNum = Math.floor(Math.random() * 3);
+
+    switch (randomNum) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors"
+        }
+    }
+
+console.log("The computer chose: " + getComputerChoice());
+console.log("Hello World");
